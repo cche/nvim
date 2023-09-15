cb7a5333343e264de5acb5d24cf1d5dc5da8ba88
Cristian <cche@users.noreply.github.com>
2023-09-15 12:00:51 +0200

    feat: Exit if neotree is the last and autoclose after opening file

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -7,7 +7,18 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+
+      }
+    }
   end,
   keys = {
     {
