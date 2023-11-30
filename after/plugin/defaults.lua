9d5ccfc55d25aabb0fa71c0cf3a482169c55934f
Cristian <cche@users.noreply.github.com>
2023-11-30 23:21:23 +0100

    Added default tab settings = 4

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -21,6 +21,13 @@ end
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
+-- set tabwidth
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+
 -- Decrease update time
 vim.o.updatetime = 250
 
