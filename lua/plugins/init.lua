11ba87933a63774897903e1b1023709a7f0ad7d4
Cristian <cche@users.noreply.github.com>
2023-11-30 23:15:49 +0100

    Added gx, nvim-surround and toggleterm plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,4 +2,35 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "chrishrb/gx.nvim",
+    event = { "BufEnter" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true, -- default settings
+  },
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end
+  },
+  -- Test documentation generation plugin at some point
+  -- {
+  --   "danymat/neogen",
+  --   dependencies = "nvim-treesitter/nvim-treesitter",
+  --   config = true
+  -- },
+  -- terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = '*',
+    config = function()
+      require("toggleterm").setup {
+        open_mapping = [[<c-\>]],
+        direction = 'float',
+      }
+    end
+  },
+}
